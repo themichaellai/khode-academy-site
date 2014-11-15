62ad24d8cce7a3293114b950a96d3f8e378f1817
Michael Lai <themichaellai@gmail.com>
2014-11-15 15:53:49 -0500

    Lesson controller

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,32 @@
+class LessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lesson_path(@lesson)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(
+      :title,
+      :lesson_text,
+      :instructions,
+      :boilerplate_code
+    )
+  end
+end
