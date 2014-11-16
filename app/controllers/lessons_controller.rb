9cc48ffd86a9969d13e892339fea506170811eea
Michael Lai <themichaellai@gmail.com>
2014-11-15 19:08:56 -0500

    Lessons edit/update

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -23,6 +23,19 @@ class LessonsController < ApplicationController
     @lesson = Lesson.new
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to @lesson
+    else
+      render 'edit'
+    end
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(
